382e4c472203a30853055637b8d550f214e98977
idcetim <javirofe96@gmail.com>
2023-05-07 17:52:05 +0200

    Add work experience favourite technologies section

--- src/routes/WorkExperience.jsx
+++ src/routes/WorkExperience.jsx
@@ -1,5 +1,25 @@
+import { FormattedMessage } from 'react-intl'
+import '../styles/WorkExperience.css'
 const WorkExperience = () => {
-
+return(
+  <div>
+    <h2 className="personal-title"><FormattedMessage id="dev-position"/></h2>
+    <section className="section-container">
+      <h3><FormattedMessage id="fav-stack" /></h3>
+      <ul>
+        <li>Front-end: <FormattedMessage id="frontend-stack" /> </li>
+        <li>Back-end: <FormattedMessage id="backend-stack" /> </li>
+        <li>Blockchain: <FormattedMessage id="blockchain-stack" /> </li>
+        <li><FormattedMessage id="git-stack" /> </li>
+        <li><FormattedMessage id="management-stack" /> </li>
+      </ul>
+    </section>
+    <h2 className="personal-title"><FormattedMessage id="home-work-experience" /></h2>
+    <section className="section-container">
+      <p></p>
+    </section>
+  </div>
+)
 }
 
 export default WorkExperience
