28af2e0102547cf83d181f7c60fd6e06f50bdbf6
idcetim <javirofe96@gmail.com>
2023-05-07 08:43:37 +0200

    Add react-intl and locales structure for automatic translations

--- src/App.js
+++ src/App.js
@@ -6,25 +6,31 @@ import WorkExperience from './routes/WorkExperience'
 import Examples from './routes/Examples'
 import GlobalHeader from './components/GlobalHeader'
 import GlobalFooter from './components/GlobalFooter'
+import { IntlProvider } from 'react-intl';
+// import enMessages from './locales/en.json';
+import espMessages from './locales/esp.json';
+
 import './styles/App.css'
 
-function App () {
+function App() {
   return (
-    <div className='global'>
-      <GlobalHeader />
-      <BrowserRouter>
-        <Routes>
-          <Route>
-            <Route path='/' element={<Home />} />
-            <Route path='/introduction' element={<Introduction />} />
-            <Route path='/work-experience' element={<WorkExperience />} />
-            <Route path='/studies' element={<Studies />} />
-            <Route path='/example' element={<Examples />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-      <GlobalFooter />
-    </div>
+    <IntlProvider locale="esp" messages={espMessages}>
+      <div className='global'>
+        <GlobalHeader />
+        <BrowserRouter>
+          <Routes>
+            <Route>
+              <Route path='/' element={<Home />} />
+              <Route path='/introduction' element={<Introduction />} />
+              <Route path='/work-experience' element={<WorkExperience />} />
+              <Route path='/studies' element={<Studies />} />
+              <Route path='/example' element={<Examples />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        <GlobalFooter />
+      </div>
+    </IntlProvider>
   )
 }
 
