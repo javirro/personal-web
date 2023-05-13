6f8ed7c8efd3c0ce00bcb6ea3b53a01095d65e42
idcetim <javirofe96@gmail.com>
2023-05-13 11:34:04 +0200

    Editar footer estilo para que el resto del contenido sea scrollable

--- src/components/GlobalFooter.jsx
+++ src/components/GlobalFooter.jsx
@@ -1,6 +1,7 @@
 import '../styles/GlobalFooter.css'
 import linkedin from '../images/linkedin.svg'
 import email from '../images/email.svg'
+import { FormattedMessage } from 'react-intl'
 
 const GlobalFooter = () => {
 
@@ -8,6 +9,7 @@ const GlobalFooter = () => {
   const linkedinHandleClick = () => window.open('https://www.linkedin.com/in/javirrof', '_blank')
   return (
     <footer>
+      <span className="contact-me"> <FormattedMessage id="contact-me" /> </span>
         <button onClick={linkedinHandleClick}><img src={linkedin} alt="linkedin icon" className='images' /></button>
         <button onClick={emailHandleClick}><img src={email} alt="email icon" className='images' /></button>
     </footer>
