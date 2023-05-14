1a1ef47e275ab002a50462d18ff8fd85feb16505
idcetim <javirofe96@gmail.com>
2023-05-14 09:11:10 +0200

    Añadir loader de work in progress

--- src/routes/Examples.jsx
+++ src/routes/Examples.jsx
@@ -1,10 +1,16 @@
+import { FormattedMessage } from 'react-intl'
+import Loader from '../components/Loader'
 import '../styles/Examples.css'
 const Examples = () => {
-return (
-  <div className="parallax">
+  return (
+    <main className="examples-container">
+      <section className="wip-container">
+        <h2 className="title-example"> <FormattedMessage id="work-in-progress" /> </h2>
+        <Loader />
+      </section>
 
-  </div>
-)
+    </main>
+  )
 }
 
 export default Examples
