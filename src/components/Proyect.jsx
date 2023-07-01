2222955de9765e10131a8b74b6e9ed851585730f
idcetim <javirofe96@gmail.com>
2023-07-01 07:52:39 +0200

    Modifying some styles in personal proyects. Change tictactoe image

--- src/components/Proyect.jsx
+++ src/components/Proyect.jsx
@@ -1,7 +1,7 @@
 import '../styles/proyect.css'
 import { PROYECTS } from '../constants'
-import game3rowImage from '../images/3row.jpg'
 import foodImage from '../images/food.jpg'
+import tictactoe from '../images/tictactoe.jpg'
 import github from '../images/githubIcon.svg'
 import { FormattedMessage } from 'react-intl'
 
@@ -10,21 +10,19 @@ const Proyect = ({ title }) => {
   const { front, back, url } = proyect
   let imgToShow
   if (title === "game3row") {
-    imgToShow = game3rowImage
+    imgToShow = tictactoe
   } else if (title === "food") {
     imgToShow = foodImage
   }
 
   return (
     <article className='proyect-container'>
-      <div className="proyect-info">
-        <a href={url} target='_blank' rel='noreferrer' className="description-title"><FormattedMessage id={`examples-${title}-title`} /></a>
-        <img src={imgToShow} alt={title} className="proyect-img" />
-        <p className="description-text"><FormattedMessage id={`examples-${title}-description`} /></p>
-        <div className='git-container'>
-          {front && <a href={front} target='_blank' rel='noreferrer' className='front-git-text'><img src={github} alt='github' className='github-img' /></a>}
-          {back && <a href={front} target='_blank' rel='noreferrer' className='back-git-text'><img src={github} alt='github' className='github-img' /></a>}
-        </div>
+      <a href={url} target='_blank' rel='noreferrer' className="description-title"><FormattedMessage id={`examples-${title}-title`} /></a>
+      <img src={imgToShow} alt={title} className="proyect-img" />
+      <p className="description-text"><FormattedMessage id={`examples-${title}-description`} /></p>
+      <div className='git-container'>
+        {front && <a href={front} target='_blank' rel='noreferrer' className='front-git-text'><img src={github} alt='github' className='github-img' /></a>}
+        {back && <a href={front} target='_blank' rel='noreferrer' className='back-git-text'><img src={github} alt='github' className='github-img' /></a>}
       </div>
     </article>)
 
--- src/routes/Examples.jsx
+++ src/routes/Examples.jsx
@@ -1,15 +1,12 @@
 import { FormattedMessage } from 'react-intl'
-import '../styles/Examples.css'
 import Proyect from '../components/Proyect'
+import '../styles/Examples.css'
+
 const Examples = () => {
   const proyects = ["food", "game3row"]
   return (
     <main className="examples-container">
-      {/* <section className="wip-container">
-        <h2 className="title-example"> <FormattedMessage id="work-in-progress" /> </h2>
-        <Loader />
-      </section> */}
-      <h2 className="title-example"> <FormattedMessage id="work-in-progress" /> </h2>
+      <h2 className="title-example"> <FormattedMessage id="examples-proyects-personal" /> </h2>
       <section className='examples-grid'>
         {proyects.map(p => <Proyect key={p} title={p} />)}
       </section>
