29fc0e9a83d1dde579a5c352108a93054a43c98b
idcetim <javirofe96@gmail.com>
2023-11-05 11:04:33 +0100

    Add new style for landing page

---
+++ src/images/landing/index.js
@@ -0,0 +1,11 @@
+import personalImg from "./personal.svg";
+import websiteImg from "./star.svg";
+import workImg from "./work.svg";
+
+const landingImgs = {
+  personalImg,
+  websiteImg,
+  workImg,
+};
+
+export default landingImgs;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,20 +1,41 @@
-import '../styles/Home.css'
 import { Link } from 'react-router-dom'
 import { FormattedMessage } from 'react-intl'
 import GlobalFooter from '../components/GlobalFooter'
+import landingImgs from '../images/landing'
+import '../styles/Home.css'
+
 
 const Home = () => {
   return (
     <>
       <main className="main-home-container">
-        <header>
-          <h1><FormattedMessage id="home-welcome" /> <span className="wave-hand"> 👋 </span></h1>
-        </header>
-        <div className="body-home-container">
-          <Link className='link' to="/personal-info"><FormattedMessage id="home-introduction" /> </Link>
-          <Link className='link' to="/work-experience"><FormattedMessage id="home-work-experience" /></Link>
-          <Link className='link' to="/example"><FormattedMessage id="home-projects" /></Link>
-        </div>
+        <h1 className="landing-title"><FormattedMessage id="home-welcome" /> <span className="wave-hand"> 👋 </span></h1>
+
+        <section className="body-home-container">
+          <div className="individual-landing-container">
+            <img src={landingImgs.personalImg} alt='personal logo' className="landing-imgs" />
+            <div className="text-container-landing">
+              <p className="texts-landings">Discover information about my studies, my passions and the stuffs that I like.</p>
+              <Link className='link' to="/personal-info"><FormattedMessage id="home-introduction" /> </Link>
+            </div>
+
+          </div>
+          <div className="individual-landing-container">
+            <div className="text-container-landing">
+              <p className="texts-landings">Discover information about my previous work experiences and my favourite tech stack.</p>
+              <Link className='link' to="/work-experience"><FormattedMessage id="home-work-experience" /></Link>
+            </div>
+            <img src={landingImgs.workImg} alt='personal logo' className="landing-imgs" />
+          </div>
+
+          <div className="individual-landing-container">
+            <img src={landingImgs.websiteImg} alt='personal logo' className="landing-imgs" />
+            <div className="text-container-landing">
+              <p className="texts-landings">Discover my best personal projects and see how they are implemented.</p>
+              <Link className='link' to="/example"><FormattedMessage id="home-projects" /></Link>
+            </div>
+          </div>
+        </section>
       </main>
       <GlobalFooter />
     </>
