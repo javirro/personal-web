9a6cca23377496a3768e118215a7336485854d84
idcetim <javirofe96@gmail.com>
2023-12-03 18:43:21 +0100

    Add route protection

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import Home from './routes/Home'
-import Introduction from './routes/Introduction'
-import WorkExperience from './routes/WorkExperience'
-import Examples from './routes/Examples'
-import GlobalHeader from './components/GlobalHeader'
-import { useSelector } from 'react-redux'
-import { IntlProvider } from 'react-intl'
-import enMessages from './locales/en.json'
-import espMessages from './locales/esp.json'
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import Home from "./routes/Home";
+import Introduction from "./routes/Introduction";
+import WorkExperience from "./routes/WorkExperience";
+import Examples from "./routes/Examples";
+import GlobalHeader from "./components/GlobalHeader";
+import { useSelector } from "react-redux";
+import { IntlProvider } from "react-intl";
+import enMessages from "./locales/en.json";
+import espMessages from "./locales/esp.json";
 
-
-import './styles/App.css'
+import "./styles/App.css";
 
 function App() {
-  const language = useSelector(s => s.language)
+  const language = useSelector((s) => s.language);
   return (
     <IntlProvider
       locale={language === "en" ? "en" : "esp"}
@@ -28,6 +27,7 @@ function App() {
               <Route path="/personal-info" element={<Introduction />} />
               <Route path="/work-experience" element={<WorkExperience />} />
               <Route path="/example" element={<Examples />} />
+              <Route path="*" element={<Navigate to="/" />} />
             </Route>
           </Routes>
         </BrowserRouter>
@@ -36,4 +36,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
